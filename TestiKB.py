f737df6edfd92d2b7b85a3aa7a88391240bdab01
inexglobal <37691139+inexglobal@users.noreply.github.com>
2019-02-26 18:16:49 +0700

    Update TestiKB.py

--- TestiKB.py
+++ TestiKB.py
@@ -1,13 +1,10 @@
 from machine import I2C,Pin
-import ipstw
 import ikb
 import time
 i2c = I2C(scl=Pin(22), sda=Pin(21), freq=1000000)
 print (i2c.scan())
 ik=ikb.IKB(i2c)
 ik.begin()
-w=ipstw.IPSTW()
-w.begin()
 while 1:
   ik.output(0,1)
   ik.motor(1,100)
